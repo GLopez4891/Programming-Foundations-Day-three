76bd27e81970af4c649728aafd78d0577dd2a0a7
Glenn Lopez <34699520+GLopez4891@users.noreply.github.com>
2018-02-12 20:47:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
-var familyTree = [];
-
-// for (var i = 0; i < familyTree.length; i++) {
-
-
 class grandParent {
-    constructor(hairColor, eyeColor, vertical) {
+    constructor(hairColor, eyeColor) {
         this.hairColor = hairColor;
         this.eyeColor = eyeColor;
-        this.vertical = vertical;
     }
-
-    jump() {
-        console.log("Grandma can jump " + this.vertical + " inches");
+    vertical() {
+        console.log("Grandma can jump 10 inches");
     }
-
 }
-let grandma = new grandParent('white', 'blue', 10);
-grandma.jump();
+
 class mother extends grandParent {
-    constructor(hairColor, eyeColor, vertical) {
-        super();
-        this.hairColor = hairColor;
-        this.eyeColor = eyeColor;
-        this.vertical = vertical;
+    vertical() {
+        console.log("Mom can jump 15 inches");
     }
-
-    jump() {
-        console.log("Mom can jump " + this.vertical + " inches");
-    }
-
 }
-let mom = new mother('blonde', 'brown', 15);
-mom.jump();
+
 class child extends grandParent {
-    constructor(hairColor, eyeColor, vertical) {
-        super();
-        this.hairColor = hairColor;
-        this.eyeColor = eyeColor;
-        this.vertical = vertical;
-    }
-
-    jump() {
-        console.log("My son can jump " + this.vertical + " inches");
+    vertical() {
+        console.log("My son can jump 25 inches");
     }
 
 }
-let son = new child('brown', 'green', 25);
-son.jump();
+
+let familyTree = [];
+
+let grandma = new grandParent("white", "blue");
+let mom = new mother("blonde", "brown");
+let son = new child("brown", "green");
+
 familyTree.push(grandma, mom, son);
-console.log(familyTree);
+
+for (let i = 0; i < familyTree.length; i++) {
+    familyTree[i].vertical();
+}
